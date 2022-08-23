aab4d63a1d725f4efab65dfdb0a55748e45e375d
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-23 22:56:56 +0530

    initial api test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const options = {
+    method: "GET",
+    url: "https://amazon-products1.p.rapidapi.com/most-wished",
+    params: { country: "US", categoryId: "165793011" },
+    headers: {
+      "X-RapidAPI-Key": "0567afe1bemsh7245822fd6f40a8p14c4d6jsna09a90df2f28",
+      "X-RapidAPI-Host": "amazon-products1.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
