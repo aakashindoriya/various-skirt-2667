d4b6d8b8f414251666896041e01bd2e81120b166
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-24 15:25:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import axios from "axios";
 function App() {
   const options = {
     method: "GET",
-    url: "https://amazon-products1.p.rapidapi.com/most-wished",
-    params: { country: "US", categoryId: "165793011" },
+    url: "https://amazon-products1.p.rapidapi.com/new-releases",
+    params: { page: 4, country: "US", categoryId: "165793011" },
     headers: {
       "X-RapidAPI-Key": "0567afe1bemsh7245822fd6f40a8p14c4d6jsna09a90df2f28",
       "X-RapidAPI-Host": "amazon-products1.p.rapidapi.com",
@@ -21,6 +21,7 @@ function App() {
     .catch(function (error) {
       console.error(error);
     });
+
   return (
     <div className="App">
       <header className="App-header"></header>
