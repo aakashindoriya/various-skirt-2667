b69222fba9e4bd07ad39ea35cca0a97613ef4a41
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-24 20:37:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 
 function App() {
   const options = {
     method: "GET",
-    url: "https://amazon-products1.p.rapidapi.com/new-releases",
-    params: { page: 4, country: "US", categoryId: "165793011" },
+    url: "https://amazon-products1.p.rapidapi.com/most-wished",
+    params: { country: "US", categoryId: "165793011" },
     headers: {
       "X-RapidAPI-Key": "0567afe1bemsh7245822fd6f40a8p14c4d6jsna09a90df2f28",
       "X-RapidAPI-Host": "amazon-products1.p.rapidapi.com",
