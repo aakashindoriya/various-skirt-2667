8d6c3ddcd030b07455a1f12b2f2987b9252da131
Aryan Dev <nobodygivesafak@gmail.com>
2022-09-08 05:05:36 +0530

    Update appcontext.jsx

--- src/Authcontext/appcontext.jsx
+++ src/Authcontext/appcontext.jsx
@@ -2,55 +2,76 @@ import { TriangleUpIcon } from "@chakra-ui/icons";
 import { useReducer } from "react";
 import { createContext } from "react";
 
-export let Authcontext=createContext()
-let initialState={
-    isAuth:false,
-    isloding:false,
-    productData:[],
-    token:null,
-    signup:true,
-    cart:[],
-    favorate:[],
-    cartTotal:null
-
+export let Authcontext = createContext();
+let initialState = {
+  isAuth: false,
+  isloding: false,
+  productData: [],
+  token: null,
+  // signup:true,
+  // cart:[],
+  // favorate:[],
+  // cartTotal:null
+  signup: true,
+  cart: [],
+  favorate: [],
+  cartTotal: null,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "LoginSuccess":
+      return {
+        ...state,
+        isAuth: true,
+        token: action.paylode,
+      };
+    case "signupSuccess":
+      return {
+        ...state,
+        signup: false,
+      };
+    case "signupUnsuccess":
+      return {
+        ...state,
+        signup: true,
+      };
+    case "addtocart":
+      return {
+        ...state,
+        cart: [...state.cart, action.paylode],
+      };
+    case "cartTotal":
+      return {
+        ...state,
+        cartTotal: action.paylode,
+      };
+    case "cartDelete":
+      return {
+        ...state,
+        cart: action.paylode,
+      };
+    case "producData":
+      return {
+        ...state,
+        productData: action.paylode,
+      };
+    case "lodingtrue":
+      return {
+        ...state,
+        isloding: true,
+      };
+    case "lodingfalse":
+      return {
+        ...state,
+        isloding: false,
+      };
+    default:
+      return state;
+  }
 }
-function reducer(state,action){
-    switch(action.type){
-        case "LoginSuccess": return {
-            ...state,isAuth:true,token:action.paylode
-        }
-        case "signupSuccess":return {
-            ...state,signup:false
-        }
-        case "signupUnsuccess":return{
-            ...state,signup:true
-        }
-        case "addtocart":return{
-            ...state,cart:[...state.cart,action.paylode]
-        }
-        case "cartTotal":return{
-            ...state,cartTotal:action.paylode
-        }
-        case "cartDelete":return{
-            ...state,cart:action.paylode
-        }
-        case "producData":return{
-            ...state,productData:action.paylode
-        }
-        case "lodingtrue":return{
-            ...state,isloding:true 
-        }
-        case "lodingfalse":return{
-            ...state,isloding:false
-        }
-        default :return state
-    }
+function AuthProvider({ children }) {
+  let [state, dispatch] = useReducer(reducer, initialState);
+  let value = { state, dispatch };
+  return <Authcontext.Provider value={value}>{children}</Authcontext.Provider>;
 }
-function AuthProvider({children}){
-let [state,dispatch]=useReducer(reducer,initialState)
-let value={state,dispatch}
-    return(
-        <Authcontext.Provider value={value}>{children}</Authcontext.Provider>
-    )
-}
-export default AuthProvider
+export default AuthProvider;
