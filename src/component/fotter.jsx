2b8328cd91764bec51f2171a208956610c6df096
Aryan Dev <nobodygivesafak@gmail.com>
2022-09-08 05:07:58 +0530

    Create fotter.jsx

---
+++ src/component/fotter.jsx
@@ -0,0 +1,15 @@
+
+import fotter from "../photo/footer.png"
+
+
+
+function FooterP(){
+    console.log("i am in footer")
+    return (
+        <div >
+           <img src={fotter} alt="not loaded" />
+        </div>
+     
+    )
+}
+export default FooterP
